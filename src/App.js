706bb32877b3469474b397f456ff68db0b8a844a
Shams <sktabrez856@gmail.com>
2023-12-05 10:05:16 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,7 @@
+import LoginRegister from "./components/login-register/login-register";
+
+function App() {
+  return <LoginRegister />;
+}
+
+export default App;
---
+++ src/components/login-register/login-register.jsx
@@ -0,0 +1,108 @@
+import React, { useState } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faGooglePlusG,
+  faFacebookF,
+  faGithub,
+  faLinkedinIn,
+} from "@fortawesome/free-brands-svg-icons";
+
+import "./style.css";
+
+const LoginRegister = () => {
+  const [active, setActive] = useState(false);
+
+  const handleRegisterClick = () => {
+    console.log("register");
+    setActive(true);
+  };
+
+  const handleLoginClick = () => {
+    console.log("login");
+    setActive(false);
+  };
+
+  console.log(active);
+
+  return (
+    <>
+      <div className={`container ${active ? "active" : ""}`} id="container">
+        <div className="form-container sign-up">
+          <form>
+            <h1>Create Account</h1>
+            <div className="social-icons">
+              <a href="#" className="icon">
+                <FontAwesomeIcon icon={faGooglePlusG} />
+              </a>
+              <a href="#" className="icon">
+                <FontAwesomeIcon icon={faFacebookF} />
+              </a>
+              <a href="#" className="icon">
+                <FontAwesomeIcon icon={faGithub} />
+              </a>
+              <a href="#" className="icon">
+                <FontAwesomeIcon icon={faLinkedinIn} />
+              </a>
+            </div>
+            <span>or use your email for registration</span>
+            <input type="text" placeholder="Name" />
+            <input type="email" placeholder="Email" />
+            <input type="password" placeholder="Password" />
+            <button>Sign Up</button>
+          </form>
+        </div>
+
+        <div className="form-container sign-in">
+          <form>
+            <h1>Sign In</h1>
+            <div className="social-icons">
+              <a href="#" className="icon">
+                <FontAwesomeIcon icon={faGooglePlusG} />
+              </a>
+              <a href="#" className="icon">
+                <FontAwesomeIcon icon={faFacebookF} />
+              </a>
+              <a href="#" className="icon">
+                <FontAwesomeIcon icon={faGithub} />
+              </a>
+              <a href="#" className="icon">
+                <FontAwesomeIcon icon={faLinkedinIn} />
+              </a>
+            </div>
+            <span>or use your email password</span>
+            <input type="email" placeholder="Email" />
+            <input type="password" placeholder="Password" />
+            <a href="#">Forget Your Password?</a>
+            <button>Sign In</button>
+          </form>
+        </div>
+        <div className="toggle-container">
+          <div className="toggle">
+            <div className="toggle-panel toggle-left">
+              <h1>Welcome Back!</h1>
+              <p>Enter your personal details to use all of site features</p>
+              <button className="hidden" id="login" onClick={handleLoginClick}>
+                Sign In
+              </button>
+            </div>
+            <div className="toggle-panel toggle-right">
+              <h1>Hello, Friend!</h1>
+              <p>
+                Register with your personal details to use all of site features
+              </p>
+              <button
+                className="hidden"
+                id="register"
+                onClick={handleRegisterClick}
+              >
+                Sign Up
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default LoginRegister;
